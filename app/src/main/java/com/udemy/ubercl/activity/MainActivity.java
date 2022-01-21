ed586cd82ed833013d30435dad736ae45124df04
Patrick <patrick.silvestre13@hotmail.com>
2022-01-21 20:41:46 -0300

    Configurando projeto e criando interface de autenticação

---
+++ app/src/main/java/com/udemy/ubercl/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.udemy.ubercl.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.udemy.ubercl.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
