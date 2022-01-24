ae2fe80f74c3ce690e827b1f5b2f9a7979fbfa1b
Patrick <patrick.silvestre13@hotmail.com>
2022-01-24 20:46:27 -0300

    Criando interface de cadastro e login

--- app/src/main/java/com/udemy/ubercl/activity/MainActivity.java
+++ app/src/main/java/com/udemy/ubercl/activity/MainActivity.java
@@ -2,7 +2,9 @@ package com.udemy.ubercl.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.udemy.ubercl.R;
 
@@ -12,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
     }
+
+    public void abrirTelaLogin(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void abrirTelaCadastro(View view){
+        startActivity(new Intent(this, CadastroActivity.class));
+    }
+
 }
