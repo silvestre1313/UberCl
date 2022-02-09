654131c5ca610bab00cfdee0e070c0dd86cec674
silvestre1313 <patrick.silvestre13@hotmail.com>
2022-02-08 21:04:12 -0300

    Recuperando localização do usuario

---
+++ app/src/main/java/com/udemy/ubercl/activity/PassageiroActivity.java
@@ -0,0 +1,143 @@
+package com.udemy.ubercl.activity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.udemy.ubercl.R;
+import com.udemy.ubercl.config.ConfiguracaoFirebase;
+
+public class PassageiroActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private FirebaseAuth autenticacao;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passageiro);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Iniciar uma viagem");
+        setSupportActionBar(toolbar);
+
+        //Configurações inicias
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        recuperaLocalizacaoUsuario();
+
+    }
+
+    private void recuperaLocalizacaoUsuario() {
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+
+                //Recuperar latitude e longitude
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                LatLng meuLocal = new LatLng(latitude, longitude);
+
+                mMap.clear();
+                mMap.addMarker(
+                        new MarkerOptions()
+                                .position(meuLocal)
+                                .title("Meu Local")
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.usuario))
+                );
+                mMap.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(meuLocal, 20)
+                );
+
+            }
+        };
+
+        //Solicitar atualização de localização
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    1000,
+                    10,
+                    locationListener
+            );
+            return;
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                autenticacao.signOut();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
